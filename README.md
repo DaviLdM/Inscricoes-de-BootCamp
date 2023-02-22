e4e61ca5c09edec3726a48312dfae6acb7bc6411
superarmax <davi.medeiros@ee.ufcg.edu.br>
2023-02-22 16:25:27 -0300

    final commit_ completed

--- README.md
+++ README.md
@@ -1,18 +1,12 @@
 ## Getting Started
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
 
-## Folder Structure
 
-The workspace contains two folders by default, where:
+## Dio Project
 
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
+The idea of ​​the project is to create a program that allows the user to enroll in bootcamps with courses and mentorships, determining the xp gained and being able to observe the completed and enrolled courses
 
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+- `Dev`: user
+- `Bootcamp`: bootcamp
 
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+using inheritance and polymorphism, encapsulation and the idea of ​​classes learned in class.
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import java.time.LocalDate;
 
+import dominio.Bootcamp;
 import dominio.Curso;
+import dominio.Dev;
 import dominio.Mentoria;
 
 public class Main {
@@ -16,7 +18,48 @@ public class Main {
         mentoria1.setDescricao("descrição mentoria java");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(mentoria1);
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(4);
+
+        //System.out.println(curso1);
+        //System.out.println(mentoria1);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devDavi = new Dev();
+        devDavi.setNome("Davi Lima");
+        devDavi.inscreverBootcamp(bootcamp);
+        System.out.println("conteudos Inscritos Davi:" + devDavi.getConteudosInscritos());
+        devDavi.progredir();
+        System.out.println("--------Progredindo----------");
+        System.out.println("conteudos Inscritos atualmente Davi:" + devDavi.getConteudosInscritos());
+        System.out.println("conteudos Concluidos Davi:" + devDavi.getConteudosConcluidos());
+        System.out.println("XP:" + devDavi.calcularTotalXp());
+
+        System.out.println("-------------------------------");
+
+        Dev devDebora = new Dev();
+        devDebora.setNome("Débora Lima");
+        devDebora.inscreverBootcamp(bootcamp);
+        System.out.println("conteudos inscritos Debora:" + devDebora.getConteudosInscritos());
+        devDebora.progredir();
+        devDebora.progredir();
+        devDebora.progredir();
+        System.out.println("--------Progredindo 3 cursos----------");
+        System.out.println("conteudos inscritos atualmente Debora:" + devDebora.getConteudosInscritos());
+        System.out.println("conteudos Concluidos Debora:" + devDebora.getConteudosConcluidos());
+        System.out.println("XP:" + devDebora.calcularTotalXp());
+        
+        
+
+
+
     }
 }
